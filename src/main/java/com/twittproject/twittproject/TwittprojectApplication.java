da36505c8749fcb3826b9eb92e747fe1f4c1dcf3
Adrian <adriusz@gmail.com>
2019-06-22 10:17:36 +0200

    intial commit

---
+++ src/main/java/com/twittproject/twittproject/TwittprojectApplication.java
@@ -0,0 +1,13 @@
+package com.twittproject.twittproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwittprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwittprojectApplication.class, args);
+	}
+
+}
